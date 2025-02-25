a2ce5884d03625a10cb2b41006401044a61370c0
Bruna Borges <bruna.lborges.matias@gmail.com>
2025-02-25 13:28:26 -0300

    consertando link api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://cadastro-de-dsuarios.onrender.com/"
+    baseURL: "https://cadastro-de-dsuarios.onrender.com"
 })
 
 export default api 
